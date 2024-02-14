3f74c87334ba00c56c93434ff9b150229da1e27d
theinnovativeone <guptasneha2099@gmail.com>
2024-02-14 23:55:40 +0530

    Name of screenshots of failed test cases

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -1,6 +1,8 @@
 package listeners;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
@@ -11,12 +13,16 @@ import base.BasePage;
 
 public class TestListener implements ITestListener{
 	
-	public void onTestPass(ITestResult result) {
+	public void onTestSuccess(ITestResult result) {
 		System.out.println("Test passed!! " + result.getMethod().getMethodName());
 	}
 
 	public void onTestFailure(ITestResult result) {
-		String targetPath = ".//FailedScreenshots//" + result.getName() + ".png";
+		LocalDateTime currentDateTime = LocalDateTime.now(); 
+//		String targetPath = System.getProperty("user.dir") + System.getProperty("file.separator") + "FailedScreenshots" + System.getProperty("file.separator") + result.getTestClass().getName() + "_" + currentDateTime + System.getProperty("file.separator") + result.getName() + ".png";
+		
+		String targetPath = System.getProperty("user.dir") + System.getProperty("file.separator") + "FailedScreenshots" + System.getProperty("file.separator") + result.getName() + ".png";
+		System.out.println("current time is: " + targetPath);
 		
 		ITestContext context = result.getTestContext();
 		WebDriver driver = (WebDriver) context.getAttribute("WebDriver");
